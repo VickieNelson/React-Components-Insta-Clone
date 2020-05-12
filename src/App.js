d54c7258b9216525f0516a1e02233f6a1bfbcd91
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-05-12 15:41:43 -0500

    added import to App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,24 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+
+import "./components/PostsContainer/PostHeader";
+import "./components/PostsContainer/LikeSection";
+import "./components/PostsContainer/Post";
+import "./components/PostsContainer/PostsPage";
+import "./components/PostsContainer/Posts.css";
+
+import "./components/CommentSection/Comment";
+import "./components/CommentSection/CommentInput";
+import "./components/CommentSection/CommentSectionContainer";
+import "./components/CommentSection/Comment.css";
+
+import "./components/SearchBar/SearchBarContainer";
+import "./components/SearchBar/SearchBar.css";
+
 // import the PostsPage and SearchBar and add them to the App
 
-
 const App = () => {
-
   return (
     <div className="App">
       {/* Add imported components here to render them */}
